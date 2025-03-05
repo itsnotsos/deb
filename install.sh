dffc14c6f276dbbecfc84de925950125ff76cb7d
Rude Spear <ghaplat@gmail.com>
2025-03-05 16:05:39 +0000

    fixup

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install wget
-wget -qO- https://raw.githubusercontent.com/itsnotsos/fagram-deb-package/master/fagram_repo.asc | sudo tee /etc/apt/trusted.gpg.d/fagram_repo.asc
-echo "deb [arch=amd64] https://raw.githubusercontent.com/ghazzor/fagram-deb-package/master/apt/repo/ bionic main" | sudo tee /etc/apt/sources.list.d/fagram.list
+wget -qO- https://raw.githubusercontent.com/itsnotsos/deb/main/fagram_repo.asc | sudo tee /etc/apt/trusted.gpg.d/fagram_repo.asc
+echo "deb [arch=amd64] https://raw.githubusercontent.com/itsnotsos/deb/main/apt/repo/ bionic main" | sudo tee /etc/apt/sources.list.d/fagram.list
 sudo apt update && sudo apt install fagram -y
